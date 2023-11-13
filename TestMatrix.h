58c4928b8cb4c82fc8763b4eba58232a401ef7d2
ChristianMBoyd <Boyd.Christian.M@gmail.com>
2023-11-12 21:29:47 -0500

    Added more detail for next steps

--- TestMatrix.h
+++ TestMatrix.h
@@ -23,5 +23,6 @@ private:
 	void checkDoubleMultiplication();
 
 	// Next: extract testing methods in Test (avoid duplicate), then implement similar functionality to generate random matrices
+	// Consider using typedef for function pointers and to create a header file converting: std::complex<double> -> Complex
 	// Then: implement addition/subtraction operators, then extend rows/columns and other related functions
 };
